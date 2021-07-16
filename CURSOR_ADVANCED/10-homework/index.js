d3449a89b8b413b1562523961647fab7a2eac8f7
VlasenkoKate <85071551+VlasenkoKate@users.noreply.github.com>
2021-07-16 20:44:26 +0300

    10 HW done

---
+++ CURSOR_ADVANCED/10-homework/index.js
@@ -0,0 +1,46 @@
+const keys = document.querySelector(".allkeys")
+const buttons = document.querySelectorAll("button")
+const audio = document.querySelectorAll("audio")
+
+
+keys.addEventListener('click', e => {
+    removeClass()
+    audio.forEach(sound => {
+        if (sound.className == e.target.id) {
+            buttons.forEach(button => {
+                if (button.id == e.target.id) {
+                    button.classList.add("sound")
+                }
+            })
+            sound.play()
+        } else {
+            sound.pause()
+            sound.currentTime = 0.0;
+        }
+    })
+})
+
+document.addEventListener('keyup', function(e) {
+    removeClass()
+    audio.forEach(sound => {
+        if (sound.className == e.code) {
+            buttons.forEach(button => {
+                if (button.id == e.code) {
+                    button.classList.add("sound")
+                }
+            })
+            sound.play()
+        } else {
+            sound.pause()
+            sound.currentTime = 0.0;
+        }
+
+    })
+})
+
+
+const removeClass = function() {
+    buttons.forEach(button => {
+        button.classList.remove("sound")
+    })
+}
